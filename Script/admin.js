b9423012740500c46e2bf36ef158899e89b913b5
Heather Mtuedwa <heathermtuedwa@gmail.com>
2024-04-18 10:08:47 +0200

    added files

--- Script/admin.js
+++ Script/admin.js
@@ -10,12 +10,12 @@ let heather = []
 }
 
 // Adding items in the empty array
-let item1 = new Autoconstructor('Bentley' , 'LUXURYCAR' , 3760900  , "https://i.postimg.cc/Sxt7PzMV/Bentley-Mulliner-Batur-1.jpg")
-let item2 = new Autoconstructor('BMW' , 'SUV' , 1961584 , "https://i.postimg.cc/3rmhJXr4/Bmw-car.jpg" ) 
-let item3 = new Autoconstructor('Porshche' , 'SUV' ,193300000 , "https://i.postimg.cc/k4xJ2z3Z/porschecayenne1-ID563.png")
-let item4 = new Autoconstructor('Bugatti' , 'SUPERCAR' ,263000000 , "https://i.postimg.cc/yN697mM0/Bugatti-Bolide-Hypercar-resize-1024x683.jpg" )
-let item5 = new Autoconstructor('Brabus' , 'SUV' ,22353638  , "https://i.postimg.cc/BnStFqm0/G-wagon.jpg")
-let item6 = new Autoconstructor('RollRoyce' , 'LUXURYCAR' ,2599000 , "https://i.postimg.cc/fb2hPs7Y/RR.jpg" )
+let item1 = new Autoconstructor('Bentley' , 'LUXURYCAR' , 3760900  , "https://i.ibb.co/YcrmYVT/Bentley-Mulliner-Batur-1.jpg")
+let item2 = new Autoconstructor('BMW' , 'SUV' , 1961584 , "https://i.ibb.co/dkHZ7j3/Bmw-car.jpg" ) 
+let item3 = new Autoconstructor('Porshche' , 'SUV' ,193300000 , "https://i.ibb.co/NsK8smQ/porschecayenne1-ID563.png")
+let item4 = new Autoconstructor('Bugatti' , 'SUPERCAR' ,263000000 , "https://i.ibb.co/YDrzx32/Bugatti-Bolide-Hypercar-resize-1024x683.jpg" )
+let item5 = new Autoconstructor('Brabus' , 'SUV' ,22353638  , "https://i.ibb.co/8rsB6Z3/G-wagon.jpg")
+let item6 = new Autoconstructor('RollRoyce' , 'LUXURYCAR' ,2599000 , "https://i.ibb.co/Nj7H4mz/RR.jpg" )
 
  
 // pushing items into array
